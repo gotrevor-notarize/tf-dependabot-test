e9b4d85c826f477e4b113174b438f449d67227ae
Trevor Morris <trevor.morris@notarize.com>
2024-11-10 14:04:42 -0500

    initial

---
+++ .github/main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_version = ">= 1.9.1"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">=5.74.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+  alias  = "us-east-1"
+}
+
+module "label-25" {
+  source  = "cloudposse/label/null"
+  version = "0.25.0"
+
+  name        = "test-name"
+  environment = "test"
+}
+
+module "label-24" {
+  source  = "cloudposse/label/null"
+  version = "0.24.1"
+
+  name        = "test-name"
+  environment = "test2"
+}
